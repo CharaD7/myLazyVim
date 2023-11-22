d01b192a642b2ef1889de82d1737cd31006a64eb
CharaD7 <jijakahn6@gmail.com>
2023-11-22 08:44:53 +0000

    Added more cutomization to lspsaga

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -6,4 +6,15 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons"
   },
+  opts = {
+    code_action = {
+      show_server_name = true,
+    },
+    ui = {
+      border = "rounded",
+    },
+    outline = {
+      win_width = 20,
+    },
+  },
 }
