dea5679fa9fa64e3d5b5d9f447800afb13a19087
CharaD7 <jijakahn6@gmail.com>
2023-11-22 10:47:57 +0000

    Added animation on scroll

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -15,4 +15,13 @@ return {
       opts.presets.inc_rename = false
     end
   },
+
+  -- Animate
+  "echasnovski/mini.animate",
+  event = "VeryLazy",
+  opts = function (_, opts)
+    opts.scroll = {
+      enable = false,
+    }
+  end,
 }
