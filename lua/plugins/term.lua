f8249c20323972edc253535d89d0e97610443932
CharaD7 <jijakahn6@gmail.com>
2023-12-22 09:02:07 +0000

    Added border color

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -6,5 +6,17 @@ return {
     float_opts = {
       border = "curved",
     },
+    highlights = {
+      FloatBorder = {
+        guifg = "#F28FAD"
+      },
+    },
+    shell = "/usr/bin/tmux",
+    winbar = {
+      enabled = true,
+      name_formatter = function(term) --  term: Terminal
+        return term.name
+      end,
+    },
   },
 }
