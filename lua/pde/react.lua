cceee09de10ef98b4e7ba655903c8e173663f55c
CharaD7 <jijakahn6@gmail.com>
2024-01-11 03:47:12 +0000

    Added pde for react

---
+++ lua/pde/react.lua
@@ -0,0 +1,16 @@
+if not require("config").pde.react then
+  return {}
+end
+
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "dsznajder/vscode-es7-javascript-react-snippets",
+      build = "yarn install --frozen-lockfile && yarn compile",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load("path")
+      end,
+    },
+  },
+}
