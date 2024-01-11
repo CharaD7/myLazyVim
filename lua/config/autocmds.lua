2f3486cb5cb2db878abd3815211b2e07997ff93b
CharaD7 <jijakahn6@gmail.com>
2024-01-11 03:14:02 +0000

    Added more autocommands to close windows using q

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -134,3 +134,42 @@ create({
     vim.cmd [[ hi PreProc gui=italic cterm=italic ]]
   end
 })
+
+-- windows to close
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup "close_with_q",
+  pattern = {
+    "OverseerForm",
+    "OverseerList",
+    "checkhealth",
+    "floggraph",
+    "fugitive",
+    "git",
+    "help",
+    "lspinfo",
+    "man",
+    "neotest-output",
+    "neotest-summary",
+    "qf",
+    "query",
+    "spectre_panel",
+    "startuptime",
+    "toggleterm",
+    "tsplayground",
+    "vim",
+    "neoai-input",
+    "neoai-output",
+    "notify",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  group = augroup "auto_format_options",
+  callback = function()
+    vim.cmd "set formatoptions-=cro"
+  end,
+})
