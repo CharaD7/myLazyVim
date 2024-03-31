1f4923b9779eaa33ea3cfe62f20aad2e9444d3d0
CharaD7 <jijakahn6@gmail.com>
2024-03-31 17:45:23 +0100

    Dapp - Add configuration for run with args

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -94,7 +94,21 @@ return {
         end,
         desc = "Step Over",
       },
-      {},
+      {
+        "<leader>da",
+        function ()
+          if vimm.fn.filereadable(".vscode/launch.json") then
+            local dap_vscode = require("dap.ext.vscode")
+            dap_vscode.load_launchjs(nil, {
+              ["pwa-node"] = js_based_languages,
+              ["chrome"] = js_based_languages,
+              ["pwa-chrome"] = js_based_languages,
+            })
+          end
+          require("dap").continue()
+        end,
+        desc = "Run with Args",
+      },
     }
   },
 }
