dfc72ebc64e8e4a5d364be28be9dae0baf77b090
CharaD7 <jijakahn6@gmail.com>
2024-04-20 21:03:04 +0100

    Enabled pyright lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -56,6 +56,10 @@ return {
             },
           },
         },
+        -- PYTHON
+        pyright = {
+          enabled = lsp == "pyright",
+        },
         -- CSS
         cssls = {},
         -- eslint
