f04d327d34eec90de3d433a7ef83fe4e7de01bbc
CharaD7 <jijakahn6@gmail.com>
2024-05-14 22:08:43 +0100

    Added more config to bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,5 +1,16 @@
 return {
   "akinsho/bufferline.nvim",
   version = "*",
-  dependencies = "nvim-tree/nvim-web-devicons"
+  dependencies = "nvim-tree/nvim-web-devicons",
+  options = {
+    numbers = "raise",
+    indicator = {
+      style = "underline",
+    },
+    diagnostics = "nvim_lsp",
+    color_icons = true,
+    show_tab_indicators = true,
+    separator_style = "thin",
+    always_show_bufferline = true,
+  },
 }
