b9fe1d9b2e78b806b1f5e0b8b803c1fcefdca526
CharaD7 <jijakahn6@gmail.com>
2024-05-15 23:50:03 +0100

    Added more tweaks to bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,16 +1,59 @@
+local numbers =
+{ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "12", "13", "14", "15", "16", "17", "18", "19", "20" }
+for _, num in pairs(numbers) do
+	vim.keymap.set("n", "<leader>" .. num, "<cmd>BufferLineGoToBuffer " .. num .. "<CR>")
+end
+
 return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
   options = {
-    numbers = "raise",
+    numbers = function(opts)
+			return string.format("%s", opts.raise(opts.ordinal))
+			-- return string.format('%s.%s', opts.ordinal, opts.raise(opts.id))
+		end,
     indicator = {
-      style = "underline",
+      style = "icon",
     },
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+			local s = " "
+			for e, n in pairs(diagnostics_dict) do
+				local sym = e == "error" and "  " or (e == "warning" and "  " or "  ")
+				s = s .. n .. sym
+			end
+			return s
+		end,
     diagnostics = "nvim_lsp",
     color_icons = true,
     show_tab_indicators = true,
     separator_style = "thin",
-    always_show_bufferline = true,
+    custom_areas = {
+			right = function()
+				local result = {}
+				local seve = vim.diagnostic.severity
+				local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
+				local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
+				local info = #vim.diagnostic.get(0, { severity = seve.INFO })
+				local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
+
+				if error ~= 0 then
+					table.insert(result, { text = "  " .. error, guifg = "#EC5241" })
+				end
+
+				if warning ~= 0 then
+					table.insert(result, { text = "  " .. warning, guifg = "#EFB839" })
+				end
+
+				if hint ~= 0 then
+					table.insert(result, { text = "  " .. hint, guifg = "#A3BA5E" })
+				end
+
+				if info ~= 0 then
+					table.insert(result, { text = "  " .. info, guifg = "#7EA9A7" })
+				end
+				return result
+			end,
+		},always_show_bufferline = true,
   },
 }
