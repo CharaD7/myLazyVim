bc5ab41738b46e81975154f53f1ba29d16cb37c2
CharaD7 <jijakahn6@gmail.com>
2024-06-17 09:56:35 +0000

    Animated guicursor: Consistent blink time interval

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,7 +13,7 @@ local options = {
   ttimeoutlen = 11,
   wildoptions = "pum",
   -- guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
-  guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
+  guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait400-blinkoff400-blinkon400-Cursor/lCursor,sm:block-blinkwait175-blinkoff175-blinkon175",
   pumblend = 15,
   winblend = 15,
 }
