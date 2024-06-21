51f3efa3dadd48b5005ec0beebc4752628bb7803
CharaD7 <jijakahn6@gmail.com>
2024-06-21 22:22:58 +0000

    Include comment to either switch to zephyr or catppuccin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      -- colorscheme = "zephyr",
+      -- colorscheme = "zephyr", -- or catppuccin
       colorscheme = "tokyonight",
     } },
     -- import any extras modules here
