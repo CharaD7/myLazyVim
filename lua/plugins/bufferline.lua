daa81d6ffc80d5d5cdd4d72f67ceb9bbe415a15a
CharaD7 <jijakahn6@gmail.com>
2024-06-24 01:30:41 +0000

    Added offsets configuration for NvimTree in BufferLine

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -62,6 +62,14 @@ return {
         end,
       },
       always_show_bufferline = true,
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          highlight = "Directory",
+          separator = true -- use a "true" to enable the default, or set your own character
+        }
+      }
     },
   }),
 }
