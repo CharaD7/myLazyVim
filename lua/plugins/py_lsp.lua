bc46d64a7e287b100754a64f0c9061006ae80e2c
CharaD7 <jijakahn6@gmail.com>
2024-07-16 15:34:51 +0000

    Added lsp configs

--- lua/plugins/py_lsp.lua
+++ lua/plugins/py_lsp.lua
@@ -4,6 +4,20 @@ return {
     require('py_lsp').setup({
       default_venv_name = "venv", -- For local venv
       host_python = "venv/bin/python3",
+      language_server = "pylsp",
+      source_strategies = {
+        "poetry",
+        "default",
+        "system"
+      },
+      capabilities = capabilities,
+      on_attach = on_attach,
+      pylsp_plugins = {
+        autopep8 = { enabled = true },
+        pyls_mypy = { enabled = true },
+        pyls_isort = { enabled = true },
+        flake8 = { enabled = true, executable = "venv/bin/flake8" },
+      },
     })
   end
 }
