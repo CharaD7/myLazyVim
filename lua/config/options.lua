be06bd22c82e1609796c8c67111d6f385fda92a5
CharaD7 <jijakahn6@gmail.com>
2024-09-12 22:45:28 +0000

    Changed append tab listchar

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -40,7 +40,9 @@ vim.opt.fillchars:append('fold:•')
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
 vim.opt.listchars:append("trail:◊")
-vim.opt.listchars:append("tab:▷▷⋮")
+vim.opt.listchars:append("tab:➡️⋮")
+
+-- vim.opt.listchars:append("tab:▷▷⋮")
 
 
 -- Neovide Configuration
