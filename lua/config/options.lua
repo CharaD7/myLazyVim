4e34da2ebd5ea8ce213c39e6630a934b8b0889b2
CharaD7 <jijakahn6@gmail.com>
2024-09-12 20:35:46 +0000

    Tweak: Enclosed neovide configuration in an if statement

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -44,29 +44,34 @@ vim.opt.listchars:append("tab:▷▷⋮")
 
 
 -- Neovide Configuration
-vim.g.neovide_fullscreen = false
-vim.g.neovide_floating_blur_amount_x = 4.0 -- works only on mac
-vim.g.floaterm_winblend = 15
-vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_theme = 'auto' -- Detect neovim set theme mode and switch accordingly
-vim.g.neovide_floating_blur_amount_y = 4.0 -- works only on mac
-vim.g.neovide_remember_window_size = true
-vim.g.neovide_transparency = 1
--- vim.g.neovide_transparency = 0.8
-vim.g.neovide_padding_top = 5
-vim.g.neovide_padding_bottom = 5
-vim.g.neovide_padding_right = 5
-vim.g.neovide_padding_left = 5
-vim.g.neovide_cursor_animation_length = 0.15
-vim.g.neovide_cursor_animate_in_insert_mode = true
-vim.g.neovide_cursor_animate_command_line = true
-vim.g.neovide_cursor_smooth_blink = true
-vim.g.neovide_cursor_unfocused_outline_width = 0.5
-vim.g.neovide_cursor_trail_length = 1.2
-vim.g.neovide_cursor_vfx_mode = "wireframe" -- railgun, torpedo, ripple, or wireframe
-vim.g.neovide_cursor_vfx_opacity = 200.0
-vim.g.neovide_cursor_vfx_particle_lifetime = 1.8
-vim.g.neovide_cursor_vfx_particle_density = 9.0
-vim.g.neovide_cursor_vfx_particle_speed = 15.0
-vim.g.neovide_cursor_vfx_particle_phase = 1.5 -- only for railgun
-vim.g.neovide_cursor_vfx_particle_curl = 1.0 -- only for railgun
+if vim.g.neovide then
+  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_text_gamma = 0.0
+  vim.g.neovide_text_contrast = 0.5
+  vim.g.neovide_fullscreen = false
+  vim.g.neovide_floating_blur_amount_x = 4.0 -- works only on mac
+  vim.g.floaterm_winblend = 15
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_theme = 'auto' -- Detect neovim set theme mode and switch accordingly
+  vim.g.neovide_floating_blur_amount_y = 4.0 -- works only on mac
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_transparency = 1
+  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_padding_top = 5
+  vim.g.neovide_padding_bottom = 5
+  vim.g.neovide_padding_right = 5
+  vim.g.neovide_padding_left = 5
+  vim.g.neovide_cursor_animation_length = 0.15
+  vim.g.neovide_cursor_animate_in_insert_mode = true
+  vim.g.neovide_cursor_animate_command_line = true
+  vim.g.neovide_cursor_smooth_blink = true
+  vim.g.neovide_cursor_unfocused_outline_width = 0.5
+  vim.g.neovide_cursor_trail_length = 1.2
+  vim.g.neovide_cursor_vfx_mode = "wireframe" -- railgun, torpedo, ripple, or wireframe
+  vim.g.neovide_cursor_vfx_opacity = 200.0
+  vim.g.neovide_cursor_vfx_particle_lifetime = 1.8
+  vim.g.neovide_cursor_vfx_particle_density = 9.0
+  vim.g.neovide_cursor_vfx_particle_speed = 15.0
+  vim.g.neovide_cursor_vfx_particle_phase = 1.5 -- only for railgun
+  vim.g.neovide_cursor_vfx_particle_curl = 1.0 -- only for railgun
+end
