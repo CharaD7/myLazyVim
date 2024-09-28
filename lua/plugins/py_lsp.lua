e9b2469c281abf8189748909ab5b04a4275ae6bc
CharaD7 <jijakahn6@gmail.com>
2024-09-28 18:43:12 +0000

    Fix: Remap virtual env path for py_lsp

--- lua/plugins/py_lsp.lua
+++ lua/plugins/py_lsp.lua
@@ -2,8 +2,8 @@ return {
   'HallerPatrick/py_lsp.nvim',
   config = function()
     require('py_lsp').setup({
-      default_venv_name = "venv", -- For local venv
-      host_python = os.getenv("VIRTUAL_ENV") .. "/bin/python3",
+      default_venv_name = ".venv", -- For local venv
+      host_python = ".venv/bin/python3",
       language_server = "pylsp",
       source_strategies = {
         "poetry",
@@ -16,7 +16,7 @@ return {
         autopep8 = { enabled = true },
         pyls_mypy = { enabled = true },
         pyls_isort = { enabled = true },
-        flake8 = { enabled = true, executable = os.getenv("VIRTUAL_ENV") .. "/bin/flake8" },
+        flake8 = { enabled = true, executable = ".venv/bin/flake8" },
       },
     })
   end
