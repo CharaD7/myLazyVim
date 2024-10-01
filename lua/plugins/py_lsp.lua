eb562c45b6589c6f482cb8251462d650ad0c8648
CharaD7 <jijakahn6@gmail.com>
2024-10-01 19:31:00 +0000

    Configured py_lsp

--- lua/plugins/py_lsp.lua
+++ lua/plugins/py_lsp.lua
@@ -2,18 +2,18 @@ return {
   'HallerPatrick/py_lsp.nvim',
   config = function()
     require('py_lsp').setup({
-      default_venv_name = ".venv", -- For local venv
-      host_python = ".venv/bin/python3",
-      language_server = {"pyright", "pylsp"},
-      source_strategies = {"system"},
-      capabilities = capabilities,
-      on_attach = on_attach,
-      pylsp_plugins = {
-        autopep8 = { enabled = true },
-        pyls_mypy = { enabled = true },
-        pyls_isort = { enabled = true },
-        flake8 = { enabled = true, executable = ".venv/bin/flake8" },
-      },
+      -- default_venv_name = ".venv", -- For local venv
+      -- host_python = ".venv/bin/python3",
+      -- language_server = {"pyright", "pylsp"},
+      -- source_strategies = {"system"},
+      -- capabilities = capabilities,
+      -- on_attach = on_attach,
+      -- pylsp_plugins = {
+      --   autopep8 = { enabled = true },
+      --   pyls_mypy = { enabled = true },
+      --   pyls_isort = { enabled = true },
+      --   flake8 = { enabled = true, executable = ".venv/bin/flake8" },
+      -- },
     })
   end
 }
