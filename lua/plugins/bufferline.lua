850c1853ac6cf6310a47eab366c3ebb8139d7944
CharaD7 <jijakahn6@gmail.com>
2024-10-11 21:12:30 +0000

    Disabled diagnostics for bufferline tab and colorising on diagnostics

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -20,49 +20,11 @@ return {
       indicator = {
         style = "underline",
       },
-      diagnostics_indicator = function(count, level, diagnostics_dict, context)
-        local s = " "
-        for e, n in pairs(diagnostics_dict) do
-          local sym = e == "error" and "   " or (e == "warning" and "   " or "   ")
-          s = s .. n .. sym
-        end
-        return s
-      end,
-      diagnostics = "nvim_lsp",
       diagnostics_update_in_insert = true,
       color_icons = true,
       sort_by = "insert_at_end", -- 'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
       show_tab_indicators = true,
       separator_style = "slope",
-      custom_areas = {
-        right = function()
-          local result = {}
-          local seve = vim.diagnostic.severity
-          local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
-          local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
-          local info = #vim.diagnostic.get(0, { severity = seve.INFO })
-          local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
-
-          local icons = require "chara.icons"
-
-          if error ~= 0 then
-            table.insert(result, { text = " " .. icons.diagnostics.Error .. error, guifg = "#EC5241" })
-          end
-
-          if warning ~= 0 then
-            table.insert(result, { text = " " .. icons.diagnostics.Warning .. warning, guifg = "#EFB839" })
-          end
-
-          if hint ~= 0 then
-            table.insert(result, { text = " " .. icons.diagnostics.Hint .. hint, guifg = "#A3BA5E" })
-          end
-
-          if info ~= 0 then
-            table.insert(result, { text = " " .. icons.diagnostics.Information .. info, guifg = "#7EA9A7" })
-          end
-          return result
-        end,
-      },
       always_show_bufferline = true,
       offsets = {
         {
