38ad460b68e48f754b9d51ec143caa333142b774
CharaD7 <jijakahn6@gmail.com>
2024-10-20 14:49:26 +0000

    Added minty - a plugin built by the creator of NvChad

---
+++ lua/plugins/minty.lua
@@ -0,0 +1,17 @@
+return {
+  "nvchad/minty",
+  cmd = { "Shades", "Huefy" },
+  lazy = true,
+  dependencies = {
+    "nvchad/volt",
+    lazy = true,
+  },
+  opts = {
+    huefy ={
+      border = true,
+    },
+    shades = {
+      border = true,
+    }
+  },
+}
