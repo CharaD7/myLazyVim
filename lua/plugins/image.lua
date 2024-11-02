b95cd3c42f25f25552fa9eeaf3b4c9beff8955ad
CharaD7 <jijakahn6@gmail.com>
2024-11-02 08:35:51 +0000

    Added extra configs for image nvim

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,5 +1,19 @@
 return {
   "3rd/image.nvim",
+  opts = {
+    backend = "tmux",
+    integrations = {},
+    max_width = 100,
+    max_height = 12,
+    max_height_window_percentage = math.huge, -- needed for good experience
+    max_width_window_percentage = math.huge,
+    window_overlap_clear_enabled = true,
+    window_overlap_clear_ft_ignore = {
+      "cmp_menu",
+      "cmp_docs",
+      "",
+    },
+  },
   config = function()
   end
 }
