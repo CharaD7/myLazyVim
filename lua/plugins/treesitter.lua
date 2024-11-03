db931000dacaf02aaedac3fb99ade69ec50d4016
CharaD7 <jijakahn6@gmail.com>
2024-11-03 20:02:51 +0000

    Refactored treesitter to accomodate molten

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,18 +21,48 @@ return {
       "vim",
       "vue"
     },
-  },
-  highlight = {
-    enable = true,
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
     -- MDX
     vim.filetype.add({
       extension = {
         mdx = "mdx",
       }
-    })
-    vim.treesitter.language.register("markdown", "mdx")
-  end
+    }),
+    vim.treesitter.language.register("markdown", "mdx"),
+  },
+  highlight = {
+    enable = true,
+  },
+  textobjects = {
+    move = {
+      enable = true,
+      set_jumps = false,
+      goto_next_start = {
+        -- other keymaps
+        ["]b"] = { query = "@code_cell.inner", desc = "next code block" },
+      },
+      goto_previous_start = {
+        -- other keymaps
+        ["]b"] = { query = "@code_cell.inner", desc = "previous code block"},
+      },
+    },
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["ib"] = { query = "@code_cell.inner", desc = "in block" },
+        ["ab"] = { query = "@code_cell.inner", desc = "around block" },
+      },
+    },
+    swap = { -- swap only works with code blocks that are under the same markdonw header
+      enable = true,
+      swap_next = {
+        -- other keymaps
+        ["<leader>sbl"] = "@code_cell.inner",
+      },
+      swap_previous = {
+        -- other keymaps
+        ["<leader>sbh"] = "@code_cell.inner",
+      },
+    },
+  },
 }
