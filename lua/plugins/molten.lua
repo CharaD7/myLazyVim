708189e5a0c5f1ad046d067686110063769afb23
CharaD7 <jijakahn6@gmail.com>
2024-11-10 19:14:21 +0000

    Refactored molten config

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -4,11 +4,14 @@ return {
   dependencies = { "3rd/image.nvim" },
   build = ":UpdateRemotePlugins",
   init = function()
-    vim.g.molten_auto_open_output = false
-    vim.g.molten_wrap_output = true
-    vim.g.molten_virt_lines_off_by_1 = true
-    vim.g.molten_image_provider = "image.nvim"
-    vim.g.molten_output_win_max_height = 20
+    local molten = require("molten")
+    molten.setup({
+      auto_open_output = false,
+      wrap_output = true,
+      virt_lines_off_by_1 = true,
+      image_provider = "image.nvim",
+      output_win_max_height = 20,
+    })
   end,
   keys = {
     { "<leader>me", "<cmd>MoltenEvaluateOperator<cr>", desc = "Evaluate Operator" },
