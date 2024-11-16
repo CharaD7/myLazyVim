cdcb246ea0afd74b1cda8a96ed03cee8a6b86d15
CharaD7 <jijakahn6@gmail.com>
2024-11-16 09:44:58 +0000

    Added siduck's showkeys plugin

---
+++ lua/plugins/showkeys.lua
@@ -0,0 +1,11 @@
+return {
+  "siduck/showkeys",
+  cmd = "ShowkeysToggle",
+  opts = {
+    exclude_modes = {"i"},
+    maxkeys = 5,
+    position = 'bottom-center',
+    timeout = 1,
+    -- more opts
+  }
+}
