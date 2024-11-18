4754aa9b4dcb9c68840e8dd51d02a3aa677a98d1
CharaD7 <jijakahn6@gmail.com>
2024-11-18 23:07:19 +0000

    Refactored lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,6 @@
 return {
+  -- for lsp features in code cells / embedded code
   {
-
-    -- for lsp features in code cells / embedded code
     'jmbuhr/otter.nvim',
     dev = false,
     dependencies = {
@@ -28,7 +27,39 @@ return {
     },
     {
       "Mte90/coq_wordpress",
-    }
+    },
+    { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
+    { -- nice loading notifications
+      -- PERF: but can slow down startup
+      'j-hui/fidget.nvim',
+      enabled = false,
+      opts = {},
+    },
+    {
+      {
+        'folke/lazydev.nvim',
+        ft = 'lua', -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+          },
+        },
+      },
+      { 'Bilal2453/luvit-meta', lazy = true }, -- optional `vim.uv` typings
+      { -- optional completion source for require statements and module annotations
+        'hrsh7th/nvim-cmp',
+        opts = function(_, opts)
+          opts.sources = opts.sources or {}
+          table.insert(opts.sources, {
+            name = 'lazydev',
+            group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+          })
+        end,
+      },
+    },
+    { 'folke/neoconf.nvim', opts = {}, enabled = false },
   },
   init = function()
     vim.g.coq_settings = {
@@ -40,6 +71,7 @@ return {
         }
       }
     }
+
   end,
   clients = {
     lsp = { enabled = true },
@@ -47,8 +79,8 @@ return {
   },
   config = function ()
     local nvim_lsp = require("nvim_lsp") -- composer global require php-stubs/wordpress-globals php-stubs/wordpress-stubs php-stubs/woocommerce-stubs php-stubs/acf-pro-stubs wpsyntex/polylang-stubs php-stubs/genesis-stubs php-stubs/wp-cli-stubs
-    local configs = require'lspconfig.configs'
-    local util = require'lspconfig.util'
+    local configs = require('lspconfig.configs')
+    local util = require('lspconfig.util')
 
     vim.lsp.set_log_level("off")
 
@@ -90,12 +122,18 @@ return {
     end
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local lsp_flags = {
+      allow_incremental_sync = true,
+      debounce_text_changes = 150,
+    }
+
     capabilities.textDocument.foldingRange = {
       dynamicRegistration = false,
       lineFoldingOnly = true
     }
 
     capabilities.textDocument.completion.completionItem.snippetSupport = true
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
     capabilities.textDocument.completion.completionItem.resolveSupport = {
       properties = {'documentation', 'detail', 'additionalTextEdits',}
     }
@@ -132,12 +170,26 @@ return {
 
     nvim_lsp.cssls.setup{
       capabilities = capabilities,
-      on_attach = on_attach
+      on_attach = on_attach,
+      flags = lsp_flags,
+    }
+
+    nvim_lsp.r_language_server.setup {
+      capabilities = capabilities,
+      flags = lsp_flags,
+      settings = {
+        r = {
+          lsp = {
+            rich_documentation = false,
+          },
+        },
+      },
     }
 
     nvim_lsp.html.setup{
       capabilities = capabilities,
       on_attach = on_attach,
+      flags = lsp_flags,
       filetpyes = {
         "html",
         "javascript",
@@ -151,6 +203,8 @@ return {
 
     nvim_lsp.bashls.setup{
       capabilities = capabilities,
+      flags = lsp_flags,
+      filetypes = { 'sh', 'bash' },
       on_attach = on_attach
     }
 
@@ -158,15 +212,40 @@ return {
       capabilities = capabilities,
       on_attach = on_attach
     }
+    nvim_lsp.dotls.setup {
+      capabilities = capabilities,
+      flags = lsp_flags,
+    }
+    nvim_lsp.ts_ls.setup {
+      capabilities = capabilities,
+      flags = lsp_flags,
+      filetypes = { 'mjs', 'js', 'javascript', 'typescript', 'ojs' },
+    }
+    nvim_lsp.yamlls.setup {
+      capabilities = capabilities,
+      flags = lsp_flags,
+      settings = {
+        yaml = {
+          schemaStore = {
+            enable = true,
+            url = '',
+          },
+        },
+      },
+    }
     require("tailwind-tools").setup({
+      capabilities = capabilities,
+      flags = lsp_flags,
     })
     nvim_lsp.typos_lsp.setup{
       capabilities = capabilities,
+      flags = lsp_flags,
       on_attach = on_attach
     }
     nvim_lsp.emmet_language_server.setup{
       capabilities = capabilities,
       on_attach = on_attach,
+      flags = lsp_flags,
       init_options = {
         html = {
           options = {
@@ -178,15 +257,47 @@ return {
     }
     nvim_lsp.gitlab_ci_ls.setup{
       capabilities = capabilities,
+      flags = lsp_flags,
       on_attach = on_attach
     }
     nvim_lsp.htmx.setup{
       capabilities = capabilities,
+      flags = lsp_flags,
       on_attach = on_attach
     }
-    nvim_lsp.jsonls.setup{}
+    nvim_lsp.jsonls.setup{
+      capabilities = capabilities,
+      flags = lsp_flags,
+    }
+
+    local function get_quarto_resource_path()
+      local function strsplit(s, delimiter)
+        local result = {}
+        for match in (s .. delimiter):gmatch('(.-)' .. delimiter) do
+          table.insert(result, match)
+        end
+        return result
+      end
+
+      local f = assert(io.popen('quarto --paths', 'r'))
+      local s = assert(f:read '*a')
+      f:close()
+      return strsplit(s, '\n')[2]
+    end
+
+    local lua_library_files = vim.api.nvim_get_runtime_file('', true)
+    local lua_plugin_paths = {}
+    local resource_path = get_quarto_resource_path()
+    if resource_path == nil then
+      vim.notify_once 'quarto not found, lua library files not loaded'
+    else
+      table.insert(lua_library_files, resource_path .. '/lua-types')
+      table.insert(lua_plugin_paths, resource_path .. '/lua-plugin/plugin.lua')
+    end
+
     nvim_lsp.lua_ls.setup {
       capabilities = capabilities,
+      flags = lsp_flags,
       on_attach = on_attach,
       on_init = function(client)
         local path = client.workspace_folders[1].name
@@ -207,48 +318,103 @@ return {
         })
       end,
       settings = {
-        Lua = {}
+        Lua = {
+          completion = {
+            callSnippet = 'Replace',
+          },
+          runtime = {
+            version = 'LuaJIT',
+            -- plugin = lua_plugin_paths, -- handled by lazydev
+          },
+          diagnostics = {
+            disable = { 'trailing-space' },
+          },
+          workspace = {
+            -- library = lua_library_files, -- handled by lazydev
+            checkThirdParty = false,
+          },
+          doc = {
+            privateName = { '^_' },
+          },
+          telemetry = {
+            enable = false,
+          },
+        }
       }
+    }
+    nvim_lsp.vimls.setup {
+      capabilities = capabilities,
+      flags = lsp_flags,
+    }
+    nvim_lsp.julials.setup {
+      capabilities = capabilities,
+      flags = lsp_flags,
     }
     nvim_lsp.marksman.setup{
       capabilities = capabilities,
-      on_attach = on_attach
+      on_attach = on_attach,
+      filetypes = { 'markdown', 'quarto' },
+      root_dir = util.root_pattern('.git', '.marksman.toml', '_quarto.yml'),
     }
     nvim_lsp.ruby_lsp.setup{
       capabilities = capabilities,
+      flags = lsp_flags,
       on_attach = on_attach
     }
     -- PYTHON
-    nvim_lsp.ruff_lsp.setup{
+    -- nvim_lsp.ruff_lsp.setup{
+    --   capabilities = capabilities,
+    --   flags = lsp_flags,
+    --   on_attach = on_attach,
+    --   trace = 'messages',
+    --   init_options ={
+    --     settings = {
+    --       logLevel = 'debug',
+    --     }
+    --   },
+    --   keys = {
+    --     {
+    --       "<leader>oi",
+    --       function ()
+    --         vim.lsp.buf.code_action({
+    --           apply = true,
+    --           context = {
+    --             only = { "source.organizeImports"},
+    --             diagnostics = {},
+    --           }
+    --         })
+    --       end,
+    --       desc = "Organize Imports",
+    --     },
+    --   },
+    -- }
+
+    -- See https://github.com/neovim/neovim/issues/23291
+    -- disable lsp watcher.
+    -- Too lags on linux for python projects
+    -- because pyright and nvim both create too many watchers otherwise
+    if capabilities.workspace == nil then
+      capabilities.workspace = {}
+      capabilities.workspace.didChangeWatchedFiles = {}
+    end
+    capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+
+    nvim_lsp.pyright.setup {
       capabilities = capabilities,
-      on_attach = on_attach,
-      trace = 'messages',
-      init_options ={
-        settings = {
-          logLevel = 'debug',
-        }
-      },
-      keys = {
-        {
-          "<leader>oi",
-          function ()
-            vim.lsp.buf.code_action({
-              apply = true,
-              context = {
-                only = { "source.organizeImports"},
-                diagnostics = {},
-              }
-            })
-          end,
-          desc = "Organize Imports",
+      flags = lsp_flags,
+      settings = {
+        python = {
+          analysis = {
+            autoSearchPaths = true,
+            useLibraryCodeForTypes = true,
+            diagnosticMode = 'workspace',
+          },
         },
       },
+      root_dir = function(fname)
+        return util.root_pattern('.git', 'setup.py', 'setup.cfg', 'pyproject.toml', 'requirements.txt')(fname) or util.path.dirname(fname)
+      end,
     }
-
-    -- require("pyright").setup({
-    --   on_attach = on_attach,
-    --   capabilities = capabilities,
-    -- })
 
     require'py_lsp'.setup({
       language_server = {"pylsp", "ruff"}, --"pylsp",
@@ -339,6 +505,10 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "stylua",
+        'black',
+        'isort',
+        'tree-sitter-cli',
+        'jupytext',
         "shfmt",
         "eslint-lsp",
         "dart-debug-adapter",
