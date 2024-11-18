1436e5d891eeb8264330d4e48ecfa2a63298dfd2
CharaD7 <jijakahn6@gmail.com>
2024-11-18 22:04:09 +0000

    Added otter nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,22 @@
 return {
+  {
+
+    -- for lsp features in code cells / embedded code
+    'jmbuhr/otter.nvim',
+    dev = false,
+    dependencies = {
+      {
+        'neovim/nvim-lspconfig',
+        'nvim-treesitter/nvim-treesitter',
+      },
+    },
+    opts = {
+      verbose = {
+        no_code_found = false,
+      }
+    },
+  },
+
   -- LSPconfig
   "neovim/nvim-lspconfig",
   dependencies = {
