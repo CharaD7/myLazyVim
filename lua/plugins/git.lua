d377368ff36549c14d18fad6883a70256067990a
CharaD7 <jijakahn6@gmail.com>
2024-11-18 21:59:24 +0000

    Set up git plugins config

---
+++ lua/plugins/git.lua
@@ -0,0 +1,53 @@
+-- git plugins
+
+return {
+  { 'sindrets/diffview.nvim' },
+
+  {
+    'lewis6991/gitsigns.nvim',
+    enabled = false,
+    config = function()
+      require('gitsigns').setup {}
+    end,
+  },
+
+  {
+    'akinsho/git-conflict.nvim',
+    init = function()
+      require('git-conflict').setup {
+        default_mappings = true,
+        disable_diagnostics = false,
+      }
+    end,
+    -- keys = {
+    --   { '<leader>gco', ':GitConflictChooseOurs<cr>' },
+    --   { '<leader>gct', ':GitConflictChooseTheirs<cr>' },
+    --   { '<leader>gcb', ':GitConflictChooseBoth<cr>' },
+    --   { '<leader>gc0', ':GitConflictChooseNone<cr>' },
+    --   { ']x', ':GitConflictNextConflict<cr>' },
+    --   { '[x', ':GitConflictPrevConflict<cr>' },
+    -- },
+  },
+  {
+    'f-person/git-blame.nvim',
+    init = function()
+      require('gitblame').setup {
+        enabled = false,
+      }
+      vim.g.gitblame_display_virtual_text = 1
+      vim.g.gitblame_enabled = 1
+    end,
+  },
+
+  { -- github PRs and the like with gh - cli
+    'pwntester/octo.nvim',
+    enabled = true,
+    cmd = 'Octo',
+    config = function()
+      require('octo').setup()
+      vim.keymap.set('n', '<leader>gpl', ':Octo pr list<cr>', { desc = 'Octo PR list' })
+      vim.keymap.set('n', '<leader>gpr', ':Octo review start<cr>', { desc = 'Octo Review' })
+    end,
+  },
+}
+
