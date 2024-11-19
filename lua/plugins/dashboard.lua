a700c1c7cc9df46639f2e517ecf13d47f12fef6d
CharaD7 <jijakahn6@gmail.com>
2024-11-19 08:40:44 +0000

    WIP: Buggy dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,7 +1,14 @@
 return {
   "folke/snacks.nvim",
-  event =  "VimEnter",
-  header = [[
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = [[
       ██████╗██╗  ██╗ █████╗ ██████╗  █████╗ ██████╗ ███████╗
       ██╔════╝██║  ██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚════██║
       ██║     ███████║███████║██████╔╝███████║██║  ██║    ██╔╝
@@ -9,31 +16,62 @@ return {
       ╚██████╗██║  ██║██║  ██║██║  ██║██║  ██║██████╔╝   ██║  
       ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝    ╚═╝  
 
-    ]],
-  sections = {
-    { section = "header" },
-    {
-      pane = 2,
-      section = "terminal",
-      cmd = "colorscript -e square",
-      height = 5,
-      padding = 1,
+        ]],
+        -- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
+        ---@type fun(cmd:string, opts:table)|nil
+        pick = nil,
+        -- Used by the `keys` section to show keymaps.
+        -- Set your curstom keymaps here.
+        -- When using a function, the `items` argument are the default keymaps.
+        ---@type snacks.dashboard.Item[]|fun(items:snacks.dashboard.Item[]):snacks.dashboard.Item[]?
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        sections = {
+          { section = "header" },
+          {
+            pane = 2,
+            section = "terminal",
+            cmd = "colorscript -e square",
+            height = 5,
+            padding = 1,
+          },
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = vim.fn.isdirectory(".git") == 1,
+            cmd = "hub status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = "startup" },
+        },
+      },
     },
-    { section = "keys", gap = 1, padding = 1 },
-    { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-    {
-      pane = 2,
-      icon = " ",
-      title = "Git Status",
-      section = "terminal",
-      enabled = vim.fn.isdirectory(".git") == 1,
-      cmd = "hub status --short --branch --renames",
-      height = 5,
-      padding = 1,
-      ttl = 5 * 60,
-      indent = 3,
+    notifier = { enabled = true, timeout = 3000 },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    styles = {
+      notification = {
+        wo = { wrap = true } -- Wrap notifications
+      }
     },
-    { section = "startup" },
+    pane_gap = 4, -- empty columns between vertical panes
+    autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
   },
 }
