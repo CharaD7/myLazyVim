eaf6765cb931106458486e68693ebe4731620ae4
CharaD7 <jijakahn6@gmail.com>
2024-11-19 00:14:43 +0000

    Added headlines for better rendering of markdown files

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -46,6 +46,35 @@ return {
     },
   },
 
+  { -- highlight markdown headings and code blocks etc.
+    'lukas-reineke/headlines.nvim',
+    enabled = false,
+    dependencies = 'nvim-treesitter/nvim-treesitter',
+    config = function()
+      require('headlines').setup {
+        quarto = {
+          query = vim.treesitter.query.parse(
+            'markdown',
+            [[
+                (fenced_code_block) @codeblock
+                ]]
+          ),
+          codeblock_highlight = 'CodeBlock',
+          treesitter_language = 'markdown',
+        },
+        markdown = {
+          query = vim.treesitter.query.parse(
+            'markdown',
+            [[
+                (fenced_code_block) @codeblock
+                ]]
+          ),
+          codeblock_highlight = 'CodeBlock',
+        },
+      }
+    end,
+  },
+
   -- notify
   {
     "rcarriga/nvim-notify",
