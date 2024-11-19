8288fb7016fb0dd0321885564662f41901d238c2
CharaD7 <jijakahn6@gmail.com>
2024-11-19 00:42:36 +0000

    Used snack in place of dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,20 +1,39 @@
 return {
-  "nvimdev/dashboard-nvim",
+  "folke/snacks.nvim",
   event =  "VimEnter",
-  opts = function(_, opts)
-    local logo = [[
+  header = [[
       ██████╗██╗  ██╗ █████╗ ██████╗  █████╗ ██████╗ ███████╗
       ██╔════╝██║  ██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚════██║
       ██║     ███████║███████║██████╔╝███████║██║  ██║    ██╔╝
       ██║     ██╔══██║██╔══██║██╔══██╗██╔══██║██║  ██║   ██╔╝ 
       ╚██████╗██║  ██║██║  ██║██║  ██║██║  ██║██████╔╝   ██║  
       ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝    ╚═╝  
-                                                              
-    ]]
 
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
-    opts.config.header = vim.split(logo, "\n")
-    vim.cmd.setlocal 'nonumber'
-    vim.wo.signcolumn = 'no'
-  end,
+    ]],
+  sections = {
+    { section = "header" },
+    {
+      pane = 2,
+      section = "terminal",
+      cmd = "colorscript -e square",
+      height = 5,
+      padding = 1,
+    },
+    { section = "keys", gap = 1, padding = 1 },
+    { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+    {
+      pane = 2,
+      icon = " ",
+      title = "Git Status",
+      section = "terminal",
+      enabled = vim.fn.isdirectory(".git") == 1,
+      cmd = "hub status --short --branch --renames",
+      height = 5,
+      padding = 1,
+      ttl = 5 * 60,
+      indent = 3,
+    },
+    { section = "startup" },
+  },
 }
