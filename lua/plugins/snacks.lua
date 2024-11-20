912e3fbec314350c86fa3fe1945bc7593fd8269d
CharaD7 <jijakahn6@gmail.com>
2024-11-20 04:47:46 +0000

    Created snacks config

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,298 @@
+local logo = [[
+      ██████╗██╗  ██╗ █████╗ ██████╗  █████╗ ██████╗ ███████╗
+      ██╔════╝██║  ██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚════██║
+      ██║     ███████║███████║██████╔╝███████║██║  ██║    ██╔╝
+      ██║     ██╔══██║██╔══██║██╔══██╗██╔══██║██║  ██║   ██╔╝ 
+      ╚██████╗██║  ██║██║  ██║██║  ██║██║  ██║██████╔╝   ██║  
+      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝    ╚═╝  
+]]
+
+logo = string.rep("\n", 4) .. logo .. "\n\n"
+
+-- Terminal Mappings
+local function term_nav(dir)
+  ---@param self snacks.terminal
+  return function(self)
+    return self:is_floating() and "<c-" .. dir .. ">" or vim.schedule(function()
+      vim.cmd.wincmd(dir)
+    end)
+  end
+end
+
+-- Use nvim dashboard as it is more stable
+local enable_nvim_dashboard = false
+
+return {
+  -- Disable toggle term and use Snacks terminal instead
+  {
+    "akinsho/toggleterm.nvim",
+    enabled = false,
+  },
+  -- Disable mini buffer remove and use Snacks buffer delete instead
+  {
+    "echasnovski/mini.bufremove",
+    enabled = false,
+  },
+  -- Disable mini cursor word and use Snacks words instead
+  {
+    "echasnovski/mini.cursorword",
+    enabled = false,
+  },
+  -- Disable nvim notify and use Snacks notify instead
+  {
+    "rcarriga/nvim-notify",
+    enabled = false,
+  },
+  -- Disable dashboard
+  {
+    "nvimdev/dashboard-nvim",
+    enabled = enable_nvim_dashboard,
+  },
+  -- Layout management
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    ---@module 'edgy'
+    ---@param opts Edgy.Config
+    opts = function(_, opts)
+      for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
+        opts[pos] = opts[pos] or {}
+        table.insert(opts[pos], {
+          ft = "snacks_terminal",
+          size = { height = 0.4 },
+          title = "%{b:snacks_terminal.id}: %{b:term_title}",
+          filter = function(_buf, win)
+            return vim.w[win].snacks_win
+              and vim.w[win].snacks_win.position == pos
+              and vim.w[win].snacks_win.relative == "editor"
+              and not vim.w[win].trouble_preview
+          end,
+        })
+      end
+    end,
+  },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      dashboard = {
+        enabled = not enable_nvim_dashboard,
+        preset = {
+          header = logo,
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
+        sections = {
+          { section = "header" },
+          {
+            pane = 2,
+            section = "terminal",
+            cmd = "colorscript -e square",
+            height = 5,
+            padding = 1,
+          },
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = vim.fn.isdirectory(".git") == 1,
+            cmd = "hub status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = "startup" },
+        },
+      },
+      bigfile = { enabled = true },
+      notifier = {
+        enabled = true,
+        --- Available style: "compact"|"fancy"|"minimal"
+        style = "fancy", -- similar to the default nvim-notify style
+      },
+      quickfile = { enabled = true },
+      statuscolumn = {
+        enabled = true,
+      },
+      words = { enabled = true },
+      styles = {
+        notification = {
+          wo = { wrap = true }, -- Wrap notifications
+        },
+        -- LazyGit
+        ---@class snacks.lazygit.Config: snacks.terminal.Opts
+        ---@field args? string[]
+        ---@field theme? snacks.lazygit.Theme
+        {
+          -- automatically configure lazygit to use the current colorscheme
+          -- and integrate edit with the current neovim instance
+          configure = true,
+          -- extra configuration for lazygit that will be merged with the default
+          -- snacks does NOT have a full yaml parser, so if you need `"test"` to appear with the quotes
+          -- you need to double quote it: `"\"test\""`
+          config = {
+            os = { editPreset = "nvim-remote" },
+            gui = { nerdFontsVersion = "3" },
+          },
+          theme_path = vim.fs.normalize(vim.fn.stdpath("cache") .. "/lazygit-theme.yml"),
+          -- Theme for lazygit
+          theme = {
+            [241]                      = { fg = "Special" },
+            activeBorderColor          = { fg = "MatchParen", bold = true },
+            cherryPickedCommitBgColor  = { fg = "Identifier" },
+            cherryPickedCommitFgColor  = { fg = "Function" },
+            defaultFgColor             = { fg = "Normal" },
+            inactiveBorderColor        = { fg = "FloatBorder" },
+            optionsTextColor           = { fg = "Function" },
+            searchingActiveBorderColor = { fg = "MatchParen", bold = true },
+            selectedLineBgColor        = { bg = "Visual" }, -- set to `default` to have no background colour
+            unstagedChangesColor       = { fg = "DiagnosticError" },
+          },
+          win = {
+            style = "lazygit",
+          },
+        },
+        -- LazyGit full screen
+        -- lazygit = {
+        --   width = 0,
+        --   height = 0,
+        -- },
+      },
+      -- Learn this tip from LazyVim
+      terminal = {
+        win = {
+          keys = {
+            nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
+            nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
+            nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
+            nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+          },
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>un",
+        function()
+          Snacks.notifier.hide()
+        end,
+        desc = "Dismiss All Notifications",
+      },
+      {
+        "<C-x>",
+        function()
+          Snacks.bufdelete()
+        end,
+        desc = "Delete Buffer",
+      },
+      {
+        "<leader>gg",
+        function()
+          Snacks.lazygit()
+        end,
+        desc = "Lazygit",
+      },
+      {
+        "<leader>go",
+        function()
+          Snacks.gitbrowse()
+        end,
+        desc = "Git Open on browser",
+      },
+      {
+        "<leader>gf",
+        function()
+          Snacks.lazygit.log_file()
+        end,
+        desc = "Lazygit Current File History",
+      },
+      {
+        "<leader>gl",
+        function()
+          Snacks.lazygit.log()
+        end,
+        desc = "Lazygit Log (cwd)",
+      },
+      {
+        "<leader>gL",
+        function()
+          Snacks.git.blame_line()
+        end,
+        desc = "Git Blame Line",
+      },
+      {
+        "<c-/>",
+        function()
+          Snacks.terminal()
+        end,
+        desc = "Toggle Terminal",
+      },
+      {
+        "<c-_>",
+        function()
+          Snacks.terminal()
+        end,
+        desc = "which_key_ignore",
+      },
+      {
+        "]]",
+        function()
+          Snacks.words.jump(vim.v.count1)
+        end,
+        desc = "Next Reference",
+        mode = { "n", "t" },
+      },
+      {
+        "[[",
+        function()
+          Snacks.words.jump(-vim.v.count1)
+        end,
+        desc = "Prev Reference",
+        mode = { "n", "t" },
+      },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          -- Setup some globals for debugging (lazy-loaded)
+          _G.dd = function(...)
+            Snacks.debug.inspect(...)
+          end
+          _G.bt = function()
+            Snacks.debug.backtrace()
+          end
+          vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+          -- Create some toggle mappings
+          Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+          Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+          Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+          Snacks.toggle.diagnostics():map("<leader>ud")
+          Snacks.toggle.line_number():map("<leader>ul")
+          Snacks.toggle
+            .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+            :map("<leader>uc")
+          Snacks.toggle.treesitter():map("<leader>uT")
+          Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+          Snacks.toggle.inlay_hints():map("<leader>uh")
+        end,
+      })
+    end,
+  },
+}
