04d075e1227f8e0effb7ada5e4f323630549732f
CharaD7 <jijakahn6@gmail.com>
2024-11-20 18:47:00 +0000

    Removed quarto from filetypes list, increased max_height to 20

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -18,7 +18,7 @@ return {
           clear_in_insert_mode = false,
           download_remote_images = true,
           only_render_image_at_cursor = false,
-          filetypes = { "markdown", "vimwiki", "quarto" }, -- markdown extensions (ie. quarto) can go here
+          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
         },
         neorg = {
           enabled = true,
@@ -36,7 +36,7 @@ return {
         },
       },
       max_width = 100, -- or nil for size
-      max_height = 12, -- or nil for no size
+      max_height = 20, -- or nil for no size
       max_width_window_percentage = math.huge,
       max_height_window_percentage = math.huge,
       window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
