05b4d2d13c4b3f6fa8d35d003e661855e393b162
CharaD7 <jijakahn6@gmail.com>
2024-11-20 18:07:03 +0000

    Refactored image.nvim to use luarocks dependencies

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,19 +1,49 @@
 return {
-  "3rd/image.nvim",
-  opts = {
-    backend = "tmux",
-    integrations = {},
-    max_width = 100,
-    max_height = 12,
-    max_height_window_percentage = math.huge, -- needed for good experience
-    max_width_window_percentage = math.huge,
-    window_overlap_clear_enabled = true,
-    window_overlap_clear_ft_ignore = {
-      "cmp_menu",
-      "cmp_docs",
-      "",
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1001, -- this plugin needs to run before anything else
+    opts = {
+      rocks = { "magick" },
     },
   },
-  config = function()
-  end
+  {
+    "3rd/image.nvim",
+    dependencies = { "luarocks.nvim" },
+    opts = {
+      backend = "kitty",
+      processor = "magick_rock", -- or "magick_cli"
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          filetypes = { "markdown", "vimwiki", "quarto" }, -- markdown extensions (ie. quarto) can go here
+        },
+        neorg = {
+          enabled = true,
+          filetypes = { "norg" },
+        },
+        typst = {
+          enabled = true,
+          filetypes = { "typst" },
+        },
+        html = {
+          enabled = false,
+        },
+        css = {
+          enabled = false,
+        },
+      },
+      max_width = 100, -- or nil for size
+      max_height = 12, -- or nil for no size
+      max_width_window_percentage = math.huge,
+      max_height_window_percentage = math.huge,
+      window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+      tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+    },
+  }
 }
