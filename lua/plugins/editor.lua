d58b13a82f75722297af9dc1cdfc64c7246f4f87
CharaD7 <jijakahn6@gmail.com>
2024-11-27 10:04:35 +0000

    Load flutter telescope extension

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -163,6 +163,7 @@ return {
       }
       telescope.setup(opts)
       require("telescope").load_extension("fzf")
+      require("telescope").load_extension("flutter")
       require("telescope").load_extension("file_browser")
     end,
   },
