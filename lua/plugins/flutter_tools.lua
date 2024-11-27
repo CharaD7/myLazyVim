353410a50484c74240495a5ffb57311b50eeb808
CharaD7 <jijakahn6@gmail.com>
2024-11-27 10:16:57 +0000

    Enabled debugger, added dap configuration for dart files

--- lua/plugins/flutter_tools.lua
+++ lua/plugins/flutter_tools.lua
@@ -10,11 +10,27 @@ return {
     decorations = {
       statusline = {
         -- Show currently running device if an application is started with a spec: device
+        app_version = true,
         device = true,
       },
     },
     debugger = { -- integrate with nvim dap + install dart code debugger
-      enabled = false
+      enabled = true,
+      register_configurations = function (path)
+        require("dap").configurations.dart = {
+          {
+            type = "dart",
+            request = "launch",
+            name = "Launch Flutter Program",
+            -- The nvim-dap plugin populates this variable with the filename of the current buffer
+            program = "${file}",
+            -- The nvim-dap plugin populates this variable with the editor's current working directory
+            cwd = "${workspaceFolder}",
+            -- This gets forwarded to the Flutter CLI tool, substitute `linux` for whatever device you wish to launch
+            toolArgs = {"-d", "linux"},
+          }
+        }
+      end
     },
     widget_guides = {
       enabled = true,
