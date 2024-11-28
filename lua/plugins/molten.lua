903b57063593ce142b91f107e0e9745aab34730a
CharaD7 <jijakahn6@gmail.com>
2024-11-28 23:51:29 +0000

    Allowed virtual text output for molten

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -6,6 +6,8 @@ return  {
       vim.g.molten_image_provider = 'image.nvim'
       vim.g.molten_output_win_max_height = 20
       vim.g.molten_auto_open_output = false
+      vim.g.molten_virt_text_output = true
+      vim.g.molten_virt_lines_off_by_1 = true
     end,
     keys = {
       { "<leader>me", ":MoltenEvaluateOperator<cr>", desc = "Evaluate Operator" },
