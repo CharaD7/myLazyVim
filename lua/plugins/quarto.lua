b79b22786cab7165e42f62e3016d44d9ed286407
CharaD7 <jijakahn6@gmail.com>
2024-11-29 21:05:46 +0000

    Moved molten config into quarto, added slime config

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -6,7 +6,7 @@ return {
     dev = false,
     opts = {
       lspFeatures = {
-        languages = {"python", "markdown"},
+        languages = {"python", "markdown", "html"},
         chunks = "all",
         diagnostics = {
           enabled = true,
@@ -19,6 +19,11 @@ return {
       codeRunner = {
         enabled = true,
         default_method = "molten",
+        ft_runners = {
+          python = 'molten',
+          markdown = 'molten',
+          html = 'molten',
+        },
       },
     },
     keys = {
@@ -76,6 +81,55 @@ return {
     },
   },
 
+  { -- send code from python/r/qmd documets to a terminal or REPL
+    -- like ipython, R, bash
+    'jpalardy/vim-slime',
+    dev = false,
+    init = function()
+      vim.b['quarto_is_python_chunk'] = false
+      Quarto_is_in_python_chunk = function()
+        require('otter.tools.functions').is_otter_language_context 'python'
+      end
+
+      vim.cmd [[
+      let g:slime_dispatch_ipython_pause = 100
+      function SlimeOverride_EscapeText_quarto(text)
+      call v:lua.Quarto_is_in_python_chunk()
+      if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk && !(exists('b:quarto_is_r_mode') && b:quarto_is_r_mode)
+      return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
+      else
+      if exists('b:quarto_is_r_mode') && b:quarto_is_r_mode && b:quarto_is_python_chunk
+      return [a:text, "\n"]
+      else
+      return [a:text]
+      end
+      end
+      endfunction
+      ]]
+
+      vim.g.slime_target = 'neovim'
+      vim.g.slime_no_mappings = true
+      vim.g.slime_python_ipython = 1
+    end,
+    config = function()
+      vim.g.slime_input_pid = false
+      vim.g.slime_suggest_default = true
+      vim.g.slime_menu_config = false
+      vim.g.slime_neovim_ignore_unlisted = true
+
+      local function mark_terminal()
+        local job_id = vim.b.terminal_job_id
+        vim.print('job_id: ' .. job_id)
+      end
+
+      local function set_terminal()
+        vim.fn.call('slime#config', {})
+      end
+      vim.keymap.set('n', '<leader>cm', mark_terminal, { desc = '[m]ark terminal' })
+      vim.keymap.set('n', '<leader>cs', set_terminal, { desc = '[s]et terminal' })
+    end,
+  },
+
   { -- paste an image from the clipboard or drag-and-drop
     'HakonHarnes/img-clip.nvim',
     event = 'BufEnter',
@@ -105,6 +159,37 @@ return {
       require('img-clip').setup(opts)
       vim.keymap.set('n', '<leader>ii', ':PasteImage<cr>', { desc = 'Insert image from clipboard' })
     end,
+  },
+
+  { -- preview equations
+    'jbyuki/nabla.nvim',
+    keys = {
+      { '<leader>qm', ':lua require"nabla".toggle_virt()<cr>', desc = 'toggle [m]ath equations' },
+    },
+  },
+
+  {
+    'benlubas/molten-nvim',
+    enabled = true,
+    build = ':UpdateRemotePlugins',
+    init = function()
+      vim.g.molten_image_provider = 'image.nvim'
+      vim.g.molten_output_win_max_height = 20
+      vim.g.molten_auto_open_output = false
+      vim.g.molten_virt_text_output = true
+      vim.g.molten_virt_lines_off_by_1 = true
+    end,
+    keys = {
+      { "<leader>me", ":MoltenEvaluateOperator<cr>", desc = "Evaluate Operator" },
+      { "<leader>mo", ":noautocmd MoltenEnterOutput<cr>", desc = "Open Output Window" },
+      { "<leader>mr", ":MoltenReevaluateCell<cr>", desc = "Re-evaluate Cell" },
+      { "<leader>mv", ":MoltenEvaluateVisual<cr>gv", mode = "v", desc = "Evaluate Visual Selection" },
+      { "<leader>mh", ":MoltenHideOutput<cr>", desc = "Close Output Window" },
+      { "<leader>md", ":MoltenDelete<cr>", desc = "Delete Molten Cell" },
+      -- The below works for html outputs
+      { "<leader>mb", ":MoltenOpenInBrowser<cr>", desc = "Open Output in Browser" },
+      { '<leader>mi', ':MoltenInit<cr>', desc = 'Molten Init' },
+    },
   },
 
 }
