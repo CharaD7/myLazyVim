20c0fbaf7249132061778d1cac858a59e38603fd
CharaD7 <jijakahn6@gmail.com>
2024-11-29 18:05:12 +0000

    Commented out smear-cursor

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -42,7 +42,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
-    { import = "lazyvim.plugins.extras.ui.smear-cursor" },
+    -- { import = "lazyvim.plugins.extras.ui.smear-cursor" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
