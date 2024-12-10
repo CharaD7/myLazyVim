0f54959c258fa4fd83a6e41d982b8ec3eeb75adb
CharaD7 <jijakahn6@gmail.com>
2024-12-10 00:17:00 +0000

    Removed redundant nvim-cmp config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -15,18 +15,4 @@ return {
 		end,
 	},
 
-	{
-		"nvim-cmp",
-		dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      table.insert(opts.sources, { name = "emoji" })
-      opts.window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered()
-      }
-      return opts
-    end,
-	},
-
 }
