9d4392ff004f4190539aee1ddac39e7aa73bb420
CharaD7 <jijakahn6@gmail.com>
2024-12-19 19:22:37 +0000

    Detached molten plugin config

---
+++ lua/plugins/molten.lua
@@ -0,0 +1,24 @@
+  return {
+    'benlubas/molten-nvim',
+    enabled = true,
+    build = ':UpdateRemotePlugins',
+    init = function()
+      vim.g.molten_image_provider = 'image.nvim'
+      vim.g.molten_output_win_max_height = 20
+      vim.g.molten_auto_open_output = false
+      vim.g.molten_virt_text_output = true
+      vim.g.molten_virt_lines_off_by_1 = true
+    end,
+    keys = {
+      { "<leader>me", ":MoltenEvaluateOperator<cr>", desc = "Evaluate Operator" },
+      { "<leader>mo", ":noautocmd MoltenEnterOutput<cr>", desc = "Open Output Window" },
+      { "<leader>mr", ":MoltenReevaluateCell<cr>", desc = "Re-evaluate Cell" },
+      { "<leader>mv", ":MoltenEvaluateVisual<cr>gv", mode = "v", desc = "Evaluate Visual Selection" },
+      { "<leader>mh", ":MoltenHideOutput<cr>", desc = "Close Output Window" },
+      { "<leader>md", ":MoltenDelete<cr>", desc = "Delete Molten Cell" },
+      -- The below works for html outputs
+      { "<leader>mb", ":MoltenOpenInBrowser<cr>", desc = "Open Output in Browser" },
+      { '<leader>mi', ':MoltenInit<cr>', desc = 'Molten Init' },
+    },
+  },
+
