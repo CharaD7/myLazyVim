5db5b9ef16a2d9c3b5647e6316b137ec3b432058
CharaD7 <jijakahn6@gmail.com>
2024-12-19 19:23:02 +0000

    Removed nabla, equation preview plugin

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -157,11 +157,5 @@ return {
     end,
   },
 
-  { -- preview equations
-    'jbyuki/nabla.nvim',
-    keys = {
-      { '<leader>qm', ':lua require"nabla".toggle_virt()<cr>', desc = 'toggle [m]ath equations' },
-    },
-  },
 }
 
