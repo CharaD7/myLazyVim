d1e3ab922ce57306eabbe316b9cd43f765818a79
CharaD7 <jijakahn6@gmail.com>
2025-02-26 22:14:50 +0000

    Added lsp config for ts_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -193,6 +193,33 @@ return {
       },
     }
 
+    nvim_lsp.ts_ls.setup{
+      capabilities = capabilities,
+      on_attach = on_attach,
+      init_options = {
+        plugins = {
+          name = '@vue/typescript-plugin',
+          location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+          languages = { 'vue' },
+        },
+      },
+      settings = {
+        typescript = {
+          tsserver = { useSyntaxServer = false },
+          inlayHints = {
+            includeInlayParameterNameHints = 'all',
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+          },
+        },
+      },
+    }
+
     nvim_lsp.html.setup{
       capabilities = capabilities,
       on_attach = on_attach,
