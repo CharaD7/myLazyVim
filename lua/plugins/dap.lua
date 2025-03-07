64274642a83599bf02cee33cdfe6ecb076733429
CharaD7 <jijakahn6@gmail.com>
2025-03-07 22:16:18 +0000

    Added a layout to dap_ui

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -272,7 +272,28 @@ return {
             remove = "d",
             edit = "e",
             repl = "r",
-            toggle = "t", gg
+            toggle = "t",
+          },
+          layouts = {
+            {
+              elements = {
+                -- Elements can be strings or table with id and size keys.
+                { id = "scopes", size = 0.25 },
+                "breakpoints",
+                "stacks",
+                "watches",
+              },
+              size = 40, -- 40 columns
+              position = "left",
+            },
+            {
+              elements = {
+                "repl",
+                "console",
+              },
+              size = 0.25, -- 25% of total lines
+              position = "bottom",
+            },
           },
         }
       },
