56c2a76c098df1ea954d1cf73d1e337b4e8330dd
CharaD7 <jijakahn6@gmail.com>
2025-03-07 18:26:03 +0000

    Added hover doc cursor hold to autocmds
    - Temporarily set updatetime
    - Call Lspsaga hover_doc on the current cursor position when updatetime
    is reached

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -111,12 +111,15 @@ create({ "DiagnosticChanged" }, {
 })
 
 -- Show diagnostic in floating window on hover
--- create({ "CursorHold", "CursorHoldI" }, {
---   callback = function()
---     vim.cmd([[ :Lspsaga hover_doc ]])
---     -- vim.cmd([[ lua vim.diagnostic.open_float(nil, { focus=false }) ]])
---   end,
--- })
+create({ "CursorHold", "CursorHoldI" }, {
+  callback = function()
+    -- Temporarily set the updatetime to 600ms
+    vim.opt.updatetime = 600
+    -- Call hover_doc on cursor hold
+    vim.cmd([[ :Lspsaga hover_doc ]])
+    -- vim.cmd([[ lua vim.diagnostic.open_float(nil, { focus=false }) ]])
+  end,
+})
 
 -- Enable autosave at every edit and on focus lost
 create({ "CursorHold", "CursorHoldI", "FocusLost" }, {
