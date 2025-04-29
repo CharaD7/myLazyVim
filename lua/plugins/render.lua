6ed4ffd7060e90863975f7eb22162732005b9ff5
CharaD7 <jijakahn6@gmail.com>
2025-04-29 03:12:34 +0000

    Added opts for render markdown plugin

--- lua/plugins/render.lua
+++ lua/plugins/render.lua
@@ -8,5 +8,35 @@ return {
       blink = { enabled = true },
       lsp = { enabled = true },
     },
+    signature = {
+      enabled = true,
+      highlight = 'RenderMarkdownSign',
+    },
+    indent = {
+      enabled = true,
+      highlight = 'RenderMarkdownIndent',
+    },
+    link = {
+      enabled = true,
+      highlight = 'RenderMarkdownLink',
+    },
+    pipe_table = { enabled = true },
+    quote = {
+      enabled = true,
+      highlight = {
+        'RenderMarkdownQuote1',
+        'RenderMarkdownQuote2',
+        'RenderMarkdownQuote3',
+        'RenderMarkdownQuote4',
+        'RenderMarkdownQuote5',
+        'RenderMarkdownQuote6',
+      },
+    },
+    checkbox = { enabled = true },
+    bullet = { enabled = true },
+    dash = { enabled = true },
+    code = { enabled = true },
+    paragraph = { enabled = true },
+    heading = { enabled = true },
   },
 }
