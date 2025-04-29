b5bebe5892fbb0ad105028a4c2b4d88590fbe1fd
CharaD7 <jijakahn6@gmail.com>
2025-04-29 05:01:01 +0000

    Added more opts for render-markdown plugin

--- lua/plugins/render.lua
+++ lua/plugins/render.lua
@@ -8,7 +8,7 @@ return {
       blink = { enabled = true },
       lsp = { enabled = true },
     },
-    signature = {
+    sign = {
       enabled = true,
       highlight = 'RenderMarkdownSign',
     },
@@ -18,9 +18,29 @@ return {
     },
     link = {
       enabled = true,
+      render_modes = true,
       highlight = 'RenderMarkdownLink',
     },
-    pipe_table = { enabled = true },
+    pipe_table = {
+      enabled = true,
+      render_modes = true,
+      preset = 'round',
+      style = 'full',
+      border = {
+        '┌', '┬', '┐',
+        '├', '┼', '┤',
+        '└', '┴', '┘',
+        '│', '─',
+      },
+      -- Gets placed in delimiter row for each column, position is based on alignment.
+      alignment_indicator = '━',
+      -- Highlight for table heading, delimiter, and the line above.
+      head = 'RenderMarkdownTableHead',
+      -- Highlight for everything else, main table rows and the line below.
+      row = 'RenderMarkdownTableRow',
+      -- Highlight for inline padding used to add back concealed space.
+      filler = 'RenderMarkdownTableFill',
+    },
     quote = {
       enabled = true,
       highlight = {
@@ -32,11 +52,103 @@ return {
         'RenderMarkdownQuote6',
       },
     },
-    checkbox = { enabled = true },
-    bullet = { enabled = true },
+    checkbox = {
+      enabled = true,
+      highlight = 'RenderMarkdownBullet',
+      render_modes = true,
+      unchecked = {
+        -- Replaces '[ ]' of 'task_list_marker_unchecked'.
+        icon = '󰄱 ',
+        -- Highlight for the unchecked icon.
+        highlight = 'RenderMarkdownUnchecked',
+        -- Highlight for item associated with unchecked checkbox.
+        scope_highlight = nil,
+      },
+      checked = {
+        -- Replaces '[x]' of 'task_list_marker_checked'.
+        icon = '󰱒 ',
+        -- Highlight for the checked icon.
+        highlight = 'RenderMarkdownChecked',
+        -- Highlight for item associated with checked checkbox.
+        scope_highlight = nil,
+      },
+    },
+    bullet = {
+      enabled = true,
+      render_modes = true,
+      icons = { '●', '○', '◆', '◇' },
+      highlight = 'RenderMarkdownBullet',
+    },
     dash = { enabled = true },
-    code = { enabled = true },
-    paragraph = { enabled = true },
-    heading = { enabled = true },
+    code = {
+      enabled = true,
+      style = 'full',
+      render_modes = true,
+      -- Highlight for code blocks.
+      highlight = 'RenderMarkdownCode',
+      -- Highlight for language, overrides icon provider value.
+      highlight_language = nil,
+      -- Highlight for border, use false to add no highlight.
+      highlight_border = 'RenderMarkdownCodeBorder',
+      -- Highlight for language, used if icon provider does not have a value.
+      highlight_fallback = 'RenderMarkdownCodeFallback',
+      -- Highlight for inline code.
+      highlight_inline = 'RenderMarkdownCodeInline',
+    },
+    paragraph = {
+      enabled = true,
+      render_modes = true,
+    },
+    heading = {
+      enabled = true,
+      render_modes = true,
+      border = true,
+      border_prefix = true,
+      border_virtual = true,
+      sign = true,
+      atx = true,
+      icons = { '󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' },
+      -- Highlight for the heading icon and extends through the entire line.
+      -- Output is evaluated by `clamp(value, context.level)`.
+    },
+    callout = {
+      -- Callouts are a special instance of a 'block_quote' that start with a 'shortcut_link'.
+      -- The key is for healthcheck and to allow users to change its values, value type below.
+      -- | raw        | matched against the raw text of a 'shortcut_link', case insensitive |
+      -- | rendered   | replaces the 'raw' value when rendering                             |
+      -- | highlight  | highlight for the 'rendered' text and quote markers                 |
+      -- | quote_icon | optional override for quote.icon value for individual callout       |
+      -- | category   | optional metadata useful for filtering                              |
+
+      note      = { raw = '[!NOTE]',      rendered = '󰋽 Note',      highlight = 'RenderMarkdownInfo',    category = 'github'   },
+      tip       = { raw = '[!TIP]',       rendered = '󰌶 Tip',       highlight = 'RenderMarkdownSuccess', category = 'github'   },
+      important = { raw = '[!IMPORTANT]', rendered = '󰅾 Important', highlight = 'RenderMarkdownHint',    category = 'github'   },
+      warning   = { raw = '[!WARNING]',   rendered = '󰀪 Warning',   highlight = 'RenderMarkdownWarn',    category = 'github'   },
+      caution   = { raw = '[!CAUTION]',   rendered = '󰳦 Caution',   highlight = 'RenderMarkdownError',   category = 'github'   },
+      -- Obsidian: https://help.obsidian.md/Editing+and+formatting/Callouts
+      abstract  = { raw = '[!ABSTRACT]',  rendered = '󰨸 Abstract',  highlight = 'RenderMarkdownInfo',    category = 'obsidian' },
+      summary   = { raw = '[!SUMMARY]',   rendered = '󰨸 Summary',   highlight = 'RenderMarkdownInfo',    category = 'obsidian' },
+      tldr      = { raw = '[!TLDR]',      rendered = '󰨸 Tldr',      highlight = 'RenderMarkdownInfo',    category = 'obsidian' },
+      info      = { raw = '[!INFO]',      rendered = '󰋽 Info',      highlight = 'RenderMarkdownInfo',    category = 'obsidian' },
+      todo      = { raw = '[!TODO]',      rendered = '󰗡 Todo',      highlight = 'RenderMarkdownInfo',    category = 'obsidian' },
+      hint      = { raw = '[!HINT]',      rendered = '󰌶 Hint',      highlight = 'RenderMarkdownSuccess', category = 'obsidian' },
+      success   = { raw = '[!SUCCESS]',   rendered = '󰄬 Success',   highlight = 'RenderMarkdownSuccess', category = 'obsidian' },
+      check     = { raw = '[!CHECK]',     rendered = '󰄬 Check',     highlight = 'RenderMarkdownSuccess', category = 'obsidian' },
+      done      = { raw = '[!DONE]',      rendered = '󰄬 Done',      highlight = 'RenderMarkdownSuccess', category = 'obsidian' },
+      question  = { raw = '[!QUESTION]',  rendered = '󰘥 Question',  highlight = 'RenderMarkdownWarn',    category = 'obsidian' },
+      help      = { raw = '[!HELP]',      rendered = '󰘥 Help',      highlight = 'RenderMarkdownWarn',    category = 'obsidian' },
+      faq       = { raw = '[!FAQ]',       rendered = '󰘥 Faq',       highlight = 'RenderMarkdownWarn',    category = 'obsidian' },
+      attention = { raw = '[!ATTENTION]', rendered = '󰀪 Attention', highlight = 'RenderMarkdownWarn',    category = 'obsidian' },
+      failure   = { raw = '[!FAILURE]',   rendered = '󰅖 Failure',   highlight = 'RenderMarkdownError',   category = 'obsidian' },
+      fail      = { raw = '[!FAIL]',      rendered = '󰅖 Fail',      highlight = 'RenderMarkdownError',   category = 'obsidian' },
+      missing   = { raw = '[!MISSING]',   rendered = '󰅖 Missing',   highlight = 'RenderMarkdownError',   category = 'obsidian' },
+      danger    = { raw = '[!DANGER]',    rendered = '󱐌 Danger',    highlight = 'RenderMarkdownError',   category = 'obsidian' },
+      error     = { raw = '[!ERROR]',     rendered = '󱐌 Error',     highlight = 'RenderMarkdownError',   category = 'obsidian' },
+      bug       = { raw = '[!BUG]',       rendered = '󰨰 Bug',       highlight = 'RenderMarkdownError',   category = 'obsidian' },
+      example   = { raw = '[!EXAMPLE]',   rendered = '󰉹 Example',   highlight = 'RenderMarkdownHint' ,   category = 'obsidian' },
+      quote     = { raw = '[!QUOTE]',     rendered = '󱆨 Quote',     highlight = 'RenderMarkdownQuote',   category = 'obsidian' },
+      cite      = { raw = '[!CITE]',      rendered = '󱆨 Cite',      highlight = 'RenderMarkdownQuote',   category = 'obsidian' },
+
+    },
   },
 }
