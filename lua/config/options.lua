19b9a1b2d42dfbf4881c33eb52b001995dc38429
CharaD7 <jijakahn6@gmail.com>
2025-07-15 09:57:54 +0000

    Remove extra spacing in tab listchar

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -56,7 +56,7 @@ vim.opt.fillchars:append('fold:•')
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
 vim.opt.listchars:append("trail:␣")
-vim.opt.listchars:append("tab:» ")
+vim.opt.listchars:append("tab:»")
 
 -- don't continue comments automagically
 -- https://neovim.io/doc/user/options.html#'formatoptions'
