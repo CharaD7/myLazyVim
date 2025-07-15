51273b539503cb05bae9f43ebeafacede20c3159
CharaD7 <jijakahn6@gmail.com>
2025-07-15 10:11:20 +0000

    Applied appropriate indentation in editor config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -14,39 +14,39 @@ return {
   },
 
   -- disables hungry features for files larget than 2MB
-  { 'LunarVim/bigfile.nvim' },
+  { "LunarVim/bigfile.nvim" },
 
   { -- format things as tables
-    'godlygeek/tabular',
+    "godlygeek/tabular",
   },
 
   { -- Autoformat
-    'stevearc/conform.nvim',
+    "stevearc/conform.nvim",
     enabled = true,
     keys = {
-      { '<leader>cf', '<cmd>lua require("conform").format()<cr>', desc = "Format" },
+      { "<leader>cf", '<cmd>lua require("conform").format()<cr>', desc = "Format" },
     },
     config = function()
-      require('conform').setup {
+      require("conform").setup({
         notify_on_error = false,
         -- format_on_save = {
         --   timeout_ms = 500,
         --   lsp_fallback = true,
         -- },
         formatters_by_ft = {
-          lua = { 'mystylua' },
-          python = { 'isort', 'black' },
-          quarto = { 'injected' },
+          lua = { "mystylua" },
+          python = { "isort", "black" },
+          quarto = { "injected" },
         },
         formatters = {
           mystylua = {
-            command = 'stylua',
-            args = { '--indent-type', 'Spaces', '--indent-width', '2', '-' },
+            command = "stylua",
+            args = { "--indent-type", "Spaces", "--indent-width", "2", "-" },
           },
         },
-      }
+      })
       -- Customize the "injected" formatter
-      require('conform').formatters.injected = {
+      require("conform").formatters.injected = {
         -- Set the options field
         options = {
           -- Set to true to ignore errors
@@ -55,19 +55,19 @@ return {
           -- A temporary file name with this extension will be generated during formatting
           -- because some formatters care about the filename.
           lang_to_ext = {
-            bash = 'sh',
-            c_sharp = 'cs',
-            elixir = 'exs',
-            javascript = 'js',
-            julia = 'jl',
-            latex = 'tex',
-            markdown = 'md',
-            python = 'py',
-            ruby = 'rb',
-            rust = 'rs',
-            teal = 'tl',
-            r = 'r',
-            typescript = 'ts',
+            bash = "sh",
+            c_sharp = "cs",
+            elixir = "exs",
+            javascript = "js",
+            julia = "jl",
+            latex = "tex",
+            markdown = "md",
+            python = "py",
+            ruby = "rb",
+            rust = "rs",
+            teal = "tl",
+            r = "r",
+            typescript = "ts",
           },
           -- Map of treesitter language to formatters to use
           -- (defaults to the value from formatters_by_ft)
@@ -78,9 +78,9 @@ return {
   },
 
   { -- generate docstrings
-    'danymat/neogen',
-    cmd = { 'Neogen' },
-    dependencies = 'nvim-treesitter/nvim-treesitter',
+    "danymat/neogen",
+    cmd = { "Neogen" },
+    dependencies = "nvim-treesitter/nvim-treesitter",
     config = true,
   },
 
@@ -100,10 +100,10 @@ return {
 
   -- interactive global search and replace
   {
-    'nvim-pack/nvim-spectre',
-    cmd = { 'Spectre' },
+    "nvim-pack/nvim-spectre",
+    cmd = { "Spectre" },
     dependencies = {
-      'nvim-lua/plenary.nvim',
+      "nvim-lua/plenary.nvim",
     },
   },
 }
