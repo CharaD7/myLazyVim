acf04c38cc9b5f411f5240e5f5dddf0d857805f4
Joy Ayitey <jijakahn6@gmail.com>
2025-08-13 22:38:12 +0000

    Added autocmd to render image files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -297,3 +297,14 @@ vim.api.nvim_create_autocmd("BufReadCmd", {
     vim.cmd("let tobedeleted = bufnr('%') | b# | exe \"bd! \" . tobedeleted")
   end
 })
+
+-- Render image files
+vim.api.nvim_create_autocmd("BufReadCmd", {
+  pattern = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" },
+  callback = function()
+    local filename = vim.fn.shellescape(vim.api.nvim_buf_get_name(0))
+    vim.cmd("silent !eyestalk " .. filename .. " &")
+    vim.cmd("let tobedeleted = bufnr('%') | b# | exe \"bd! \" . tobedeleted")
+  end
+})
+
