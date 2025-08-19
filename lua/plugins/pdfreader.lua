d6bcf88a46654c47b06ff4d28ea3f9132b318444
Joy Ayitey <jijakahn6@gmail.com>
2025-08-19 10:57:05 +0000

    Removed telescope dependency on pdfreader

--- lua/plugins/pdfreader.lua
+++ lua/plugins/pdfreader.lua
@@ -3,7 +3,6 @@ return {
 	lazy = false,
 	dependencies = {
 		"folke/snacks.nvim", -- image rendering
-		"nvim-telescope/telescope.nvim", -- pickers
 	},
 	config = function()
 		require("pdfreader").setup()
