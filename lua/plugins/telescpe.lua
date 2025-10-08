d226e104f8bd3862edf37012bcae524527cba495
Joy Ayitey <jijakahn6@gmail.com>
2025-10-08 15:18:24 +0000

    Added telescope  plugin

--- lua/plugins/telescpe.lua
+++ lua/plugins/telescpe.lua
@@ -1,4 +1,8 @@
 return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	dependencies = { {'nvim-lua/plenary.nvim'} }
+	dependencies = { {'nvim-lua/plenary.nvim'} },
+	config = function()
+    local telescope = require('telescope')
+		telescope.load_extension('noice')
+	end
 }
